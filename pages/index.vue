0a5073ac1dabf517a6b8025650d1d4c61557aa60
miracleonyenma <miracleficient@gmail.com>
2022-03-03 10:43:04 +0100

    update: manually add images

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,11 @@
 <template>
 	<div class="home">
 		<section id="site-hero" class="relative p-4 py-16">
-			<div
-				class="wrapper grid md:grid-cols-5 items-center gap-6 min-h-[28rem] max-w-6xl m-auto"
-			>
-				<header
-					class="hero-header col-span-3 grid grid-cols-3 items-center h-full min-h-[15rem]"
-				>
-					<div
-						class="shape-bg-wrapper absolute top-0 left-0 w-full h-full"
-					></div>
-					<div
-						class="cont col-span-2 flex flex-col justify-between h-full max-h-28 md:max-h-60"
-					>
-						<h1
-							class="font-heading font-black text-5xl text-white tracking-tight mb-4 md:text-7xl z-10"
-						>
+			<div class="wrapper grid md:grid-cols-5 items-center gap-6 min-h-[28rem] max-w-6xl m-auto">
+				<header class="hero-header col-span-3 grid grid-cols-3 items-center h-full min-h-[15rem]">
+					<div class="shape-bg-wrapper absolute top-0 left-0 w-full h-full"></div>
+					<div class="cont col-span-2 flex flex-col justify-between h-full max-h-28 md:max-h-60">
+						<h1 class="font-heading font-black text-5xl text-white tracking-tight mb-4 md:text-7xl z-10">
 							{{ details.name.first }}
 							<br />
 							{{ details.name.last }}
@@ -26,16 +16,12 @@
 								:key="social.name"
 								class="text-indigo-200 hover:text-white hover:underline underline-offset-8"
 							>
-								<a :href="social.url" target="_blank" rel="noopener">{{
-									social.name
-								}}</a>
+								<a :href="social.url" target="_blank" rel="noopener">{{ social.name }}</a>
 							</li>
 						</ul>
 					</div>
 					<figure class="relative col-span-1 h-full">
-						<div
-							class="img-cont absolute top-0 right-0 h-full w-[11.5rem] md:w-[200%] lg:w-[170%] overflow-y-hidden"
-						>
+						<div class="img-cont absolute top-0 right-0 h-full w-[11.5rem] md:w-[200%] lg:w-[170%] overflow-y-hidden">
 							<img src="~/assets/img/patrick.png" alt="Picture of wale" />
 						</div>
 					</figure>
@@ -48,9 +34,7 @@
 						{{ details.introduction.caption }}
 					</h3>
 					<p>{{ details.introduction.content }}</p>
-					<button
-						class="cta flex items-center gap-2 mt-4 font-semibold text-indigo-700 bg-transparent pl-0"
-					>
+					<button class="cta flex items-center gap-2 mt-4 font-semibold text-indigo-700 bg-transparent pl-0">
 						More about me
 						<icon :icon="arrowNarrowRight" :mode="'outline'" />
 					</button>
@@ -61,36 +45,25 @@
 		</section>
 
 		<section class="site-section">
-			<div class="wrapper md:max-w-4xl m-auto">
+			<div class="wrapper relative md:max-w-4xl m-auto">
 				<h2 class="md:pl-6 mb-2">&mdash; Projects</h2>
 
 				<header class="mb-4 grid gap-4 md:grid-cols-2">
 					<div class="wrapper md:p-6 md:min-h-[20rem]">
 						<h3 class="text-2xl mb-4">All creative Works, selected projects</h3>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-							molestiae praesentium vero.
-						</p>
-						<button
-							class="cta flex items-center gap-2 mt-4 font-semibold text-indigo-700 bg-transparent pl-0"
-						>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.</p>
+						<button class="cta flex items-center gap-2 mt-4 font-semibold text-indigo-700 bg-transparent pl-0">
 							Explore more
 							<icon :icon="arrowNarrowRight" :mode="'outline'" />
 						</button>
 					</div>
 				</header>
 
-				<ul
-					class="projects grid gap-4 md:grid-cols-2 transform md:-translate-y-1/2"
-				>
+				<ul class="projects absolute grid gap-4 md:grid-cols-2 transform md:top-0">
 					<li class="project md:min-h-[20rem]">
 						<div class="wrapper"></div>
 					</li>
-					<li
-						v-for="project in projects"
-						:key="project.id"
-						class="project md:p-2 min-h-[20rem]"
-					>
+					<li v-for="project in projects" :key="project.id" class="project md:p-2 min-h-[20rem]">
 						<div
 							class="transition-all wrapper flex flex-col bg-white w-full h-full transform md:hover:scale-110 rounded-xl shadow"
 						>
@@ -106,9 +79,7 @@
 								<p>{{ project.description }}</p>
 							</header>
 
-							<div
-								class="media-cont relative flex-grow h-fit overflow-hidden rounded-b-xl"
-							>
+							<div class="media-cont relative flex-grow h-fit overflow-hidden rounded-b-xl">
 								<div class="img-cont h-[13rem] w-[110%]">
 									<img
 										:src="project.img"
@@ -140,8 +111,7 @@
 				},
 				introduction: {
 					caption: "Product, UI/UX designer and developer based in NIgeria",
-					content:
-						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laboriosam assumenda quis.",
+					content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laboriosam assumenda quis.",
 				},
 				socials: [
 					{ name: "TW", url: "https://twitter.com/miracleio" },
@@ -152,25 +122,53 @@
 
 			const projects = ref([
 				{
-					id: "001",
-					name: "BeServer",
-					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
-					categories: ["Branding", "product"],
-					img: "/img/download (3).png",
-				},
-				{
 					id: "002",
-					name: "Seone",
+					name: "Converge App",
 					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
 					categories: ["Product", "Development"],
-					img: "/img/3D illustrations by Morflax.png",
+					img: "/img/IMG_0225.png",
 				},
 				{
 					id: "003",
 					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
 					name: "Energetic Tumbler",
 					categories: ["Branding", "Product", "Development"],
-					img: "/img/Pitch.png",
+					img: "/img/IMG_0227.png",
+				},
+				{
+					id: "004",
+					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
+					name: "Energetic Tumbler",
+					categories: ["Branding", "Product", "Development"],
+					img: "/img/IMG_0229.png",
+				},
+				{
+					id: "005",
+					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
+					name: "Energetic Tumbler",
+					categories: ["Branding", "Product", "Development"],
+					img: "/img/IMG_6083.png",
+				},
+				{
+					id: "006",
+					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
+					name: "Energetic Tumbler",
+					categories: ["Branding", "Product", "Development"],
+					img: "/img/IMG_6095.png",
+				},
+				{
+					id: "007",
+					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
+					name: "Energetic Tumbler",
+					categories: ["Branding", "Product", "Development"],
+					img: "/img/IMG_6096.png",
+				},
+				{
+					id: "008",
+					description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae praesentium vero.`,
+					name: "Energetic Tumbler",
+					categories: ["Branding", "Product", "Development"],
+					img: "/img/IMG_6945.png",
 				},
 			]);
 
